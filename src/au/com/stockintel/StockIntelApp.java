ef1ec7d4fba29f4079ab9fcd89e6c1267b3a4eeb
Charith De Silva <charith.code@gmail.com>
2019-02-03 10:31:42 +1100

    clean up

--- src/au/com/stockintel/StockIntelApp.java
+++ src/au/com/stockintel/StockIntelApp.java
@@ -22,14 +22,18 @@ public class StockIntelApp {
 
     public void printStocks(List<Stock> listOfStocks) {
         Long startTime = new Date().getTime();
+        List<Stock> duplicateList = new ArrayList<>(listOfStocks);
+        Comparator<Stock> stockComparator = Comparator.comparing(Stock::getStockPrice);
+
         // in case of duplicated max stock values, stream.max resolves the first index.
         // It's problematic for scenarios like this -> {10,12,22,2,22,9}
         // Buy should be at 2 and Sell should be at 22.
-        List<Stock> duplicateList = new ArrayList<>(listOfStocks);
+        // Therefore, reverse the order and finds the max index.
         Collections.sort(listOfStocks, Collections.reverseOrder(Comparator.comparing(Stock::getMinute)));
+        Stock maxStock = getStockStream(listOfStocks).max(stockComparator).get();
 
-        Stock maxStock = getStockStream(listOfStocks).max(Comparator.comparing(Stock::getStockPrice)).get();
-        Stock minStock = getStockStream(duplicateList).limit(maxStock.getMinute()).min(Comparator.comparing(Stock::getStockPrice)).get();
+        // runs the min on original list, up till max index.
+        Stock minStock = getStockStream(duplicateList).limit(maxStock.getMinute()).min(stockComparator).get();
 
         System.out.println();
         System.out.println("-------------------------------------------------------------------");
@@ -42,6 +46,7 @@ public class StockIntelApp {
     private Stream<Stock> getStockStream(List<Stock> listOfStocks) {
         return listOfStocks.stream().parallel();
     }
+
 
 
 }
